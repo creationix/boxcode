a49398ab0b6cdfefd2473d2968614346f554d3cd
Tim Caswell <tim@creationix.com>
2013-01-03 16:20:27 -0600

    Remove old keyboard code

--- index.js
+++ index.js
@@ -34,44 +34,4 @@ window.addEventListener("load", function () {
   loaddir("lib");
   loaddir(".");
 
-  document.addEventListener('keydown', function (evt) {
-    if (!(evt.ctrlKey || evt.altKey)) return;
-    var orientation;
-    var action;
-    var index;
-    switch (evt.keyCode) {
-      case 49: index = 0; break;
-      case 50: index = 1; break;
-      case 51: index = 2; break;
-      case 52: index = 3; break;
-      case 53: index = 4; break;
-      case 54: index = 5; break;
-      case 55: index = 6; break;
-      case 56: index = 7; break;
-      case 57: index = 8; break;
-      case 48: index = 9; break;
-      case 78: action = "create"; break;
-      case 87: action = "close"; break;
-    }
-    if (action && evt.ctrlKey) {
-      evt.preventDefault();
-      evt.stopPropagation();
-      actions[action]();
-      return false;
-    }
-    if (index !== undefined && evt.altKey) {
-      var leaf = body.focusedLeaf;
-      if (leaf) {
-        var tab = leaf.tabs[index];
-        if (tab) {
-          evt.preventDefault();
-          evt.stopPropagation();
-          leaf.select(tab);
-          return false;
-        }
-      }
-    }
-  }, true);
-
-
 }, false);
